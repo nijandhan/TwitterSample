7f266a315284b4f04576cc56219ed4c33b7f11c4
nijandhan <nijandhanl@columbusagain.com>
2018-01-04 08:30:09 +0530

    Initial commit

---
+++ app/src/main/java/com/tarkalabs/twittersample/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.tarkalabs.twittersample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login_button) void onLoginButtonClick(){
+        Toast.makeText(this,"Login button click",Toast.LENGTH_LONG).show();
+    }
+}
